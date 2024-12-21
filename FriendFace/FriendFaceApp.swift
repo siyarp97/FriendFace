ab36fa3ea0814dbccdcf0ecfe00081b1a7b36867
Siyar <siyarp97@gmail.com>
2024-12-21 03:45:24 +0300

    Initial Commit

---
+++ FriendFace/FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceApp.swift
+//  FriendFace
+//
+//  Created by Şiyar Palabıyık on 21.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
